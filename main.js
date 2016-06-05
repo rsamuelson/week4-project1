0acfb04567a0d872153629aba0b3a517fd4ca90d
Jenny Allar <jennyallar@gmail.com>
2016-06-05 11:26:06 -0600

    splitting exercises

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+/*
+=======================================================
+
+** Week 4 - Project 1 **
+*** jQuery! ***
+
+For any methods that we didn't cover, please refer to the jQuery documentation.
+https://api.jquery.com/
+
+BASIC TRACK: 1-8
+ADVANCED TRACK: 9-12
+=======================================================
+*/
+
 // Add your JS here.
